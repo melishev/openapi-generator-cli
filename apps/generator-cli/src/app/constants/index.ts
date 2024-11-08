47afe222e02fedea27ca520479742f63cfa37813
Matvey Melishev <melishevm@gmail.com>
2024-11-08 03:33:51 +0100

    remove trash

--- apps/generator-cli/src/app/constants/index.ts
+++
@@ -1,4 +0,0 @@
-export type LOGGER = typeof console
-export const LOGGER = Symbol('LOGGER')
-
-export const COMMANDER_PROGRAM = Symbol('COMMANDER_PROGRAM')
--- apps/generator-cli/src/app/services/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './ui.service'
-export * from './config.service'
-export * from './generator.service'
-export * from './pass-through.service'
-export * from './version-manager.service'
